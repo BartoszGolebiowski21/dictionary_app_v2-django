af4d7bc571908a2a76c9d8cff286ac4cda6256db
Bartosz Golebiowski <golebiowski.bartosz.21@gmail.com>
2023-09-07 21:30:26 +0200

    Add file: 'check.html', update file: 'views.py' - check mode works, commented code has been deleted.

--- dict_project/dict_app/views.py
+++ dict_project/dict_app/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-# from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.views import View
 from django.urls import reverse, reverse_lazy
 from django.db.models import Q
@@ -26,21 +26,6 @@ class AddWordView(CreateView):
     form_class = WordForm
     template_name = "dict_app/add_word.html"
     success_url = "entire-dictionary"
-
-
-# class AddWordView(View):
-#     def get(self, request):
-#         print("funkcja get się uruchamia")
-#         form = WordForm()
-#         return render(request, "dict_app/add_word.html", {'form': form})
-    
-#     def post(self, request):
-#         print("funkcja post się uruchamia")
-#         form = WordForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return HttpResponseRedirect("add-word")
-#         return render(request, "dict_app/add_word.html", {'form': form})
 
 
 class SingleWordView(DetailView):
@@ -75,30 +60,35 @@ def search(request):
     return render(request, "dict_app/search.html", context)
 
 
-# class TestView(View):
-#     def draw_word(self, request):
-#         word = Word.objects.order_by('?')[1]
-#         return word
-    
-#     def get(self, request):
-
-
-def test(request):
-    drawn_word = Word.objects.order_by('?')[1]
-    if request.method == 'GET':
-        form = TestForm(request.GET)
-        if form.is_valid():
-            print("Słowo z formularza jest obsługiwane")
-            entered_word = form.cleaned_data["entered_word"]
-            print(entered_word)
-            print(drawn_word.english_translation)
-            context = {
-                "drawn_word": drawn_word, 
-                "entered_word": entered_word, 
-                "form": form
-                }
-            return render(request, "dict_app/test.html", context)        
-    else:
+class TestView(View):
+    def get(self, request):
         form = TestForm()
-    context = {'drawn_word': drawn_word, "form": form}
-    return render(request, "dict_app/test.html", context)
+        drawn_word = Word.objects.order_by('?')[1]
+
+        request.session['drawn_word_english'] = drawn_word.english_translation
+        request.session['drawn_word_polish'] = drawn_word.polish_translation
+
+        context = {'form': form, 'drawn_word': drawn_word}
+        return render(request, "dict_app/test.html", context)
+    
+    def post(self, request):
+        form = TestForm(request.POST)
+        if form.is_valid():
+            entered_word = form.cleaned_data["entered_word"]
+            request.session['entered_word'] = entered_word
+
+            return HttpResponseRedirect("/check")
+        return render(request, "dict_app/test.html", {'form': form})
+
+
+def check(request):
+    drawn_word_english = request.session.get('drawn_word_english')
+    drawn_word_polish = request.session.get('drawn_word_polish')
+    entered_word = request.session.get('entered_word')
+
+    context = {
+        "drawn_word_english": drawn_word_english, 
+        "drawn_word_polish": drawn_word_polish,
+        "entered_word": entered_word,
+        }
+    return render(request, "dict_app/check.html", context)
